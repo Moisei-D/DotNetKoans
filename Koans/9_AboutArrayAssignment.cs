e7856dfb29bd5b9296497c4524a4eb5b3d2ef548
Moisei-D <daniel.moisei@isa.utm.md>
2026-07-12 22:32:20 +0300

    finished 9

--- Koans/9_AboutArrayAssignment.cs
+++ Koans/9_AboutArrayAssignment.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using DotNetKoans.Engine;
+using System;
 
 namespace DotNetKoans.Koans;
 
@@ -18,7 +19,7 @@ public class AboutArrayAssignment : Koan
 		//Even though we don't specify types explicitly, the compiler
 		//will pick one for us
 		var name = "John";
-		Assert.Equal(typeof(FillMeIn), name.GetType());
+		Assert.Equal(typeof(string), name.GetType());
 
 		//but only if it can. So this doesn't work
 		//var array = null;
@@ -33,7 +34,7 @@ public class AboutArrayAssignment : Koan
 		//Even though we don't specify types explicitly, the compiler
 		//will pick one for us
 		var names = new[] { "John", "Smith" };
-		Assert.Equal(typeof(FillMeIn), names.GetType());
+		Assert.Equal(typeof(string[]), names.GetType());
 
 		//but only if it can. So this doesn't work
 		//var array = new[] { "John", 1 };
@@ -45,7 +46,7 @@ public class AboutArrayAssignment : Koan
 		//You can do multiple assignments on one line, but you 
 		//still have to be explicit
 		string firstName = "John", lastName = "Smith";
-		Assert.Equal(FILL_ME_IN, firstName);
-		Assert.Equal(FILL_ME_IN, lastName);
+		Assert.Equal("John", firstName);
+		Assert.Equal("Smith", lastName);
 	}
 }
